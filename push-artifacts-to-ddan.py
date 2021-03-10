0d7e5207d2e64cf0e2ca840f5cadeb7ed1eed7e4
George Davis <george_davis@trendmicro.com>
2021-03-10 12:34:55 -0500

    Adding rotating logger by day to the script to generate runtime logs

--- push-artifacts-to-ddan.py
+++ push-artifacts-to-ddan.py
@@ -3,11 +3,26 @@ import os
 import os.path
 import subprocess
 import boto3
+import logging
+import time
+from logging.handlers import TimedRotatingFileHandler
 
 ddanInDirFolder = '~/ddan/work/indir/'
 
-def clearDdanInDir():
+def create_timed_rotating_log(path):
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+    
+    handler = TimedRotatingFileHandler(path,
+                                       when="d",
+                                       interval=1,
+                                       backupCount=7)
+    logger.addHandler(handler)
+    return logger
+
+def clearDdanInDir(logger):
     if os.path.exists(ddanInDirFolder):
+        logger.info("Cleaning DDAN indir folder (" + ddanInDirFolder + ")...")
         os.rmdir(ddanInDirFolder)
 
 def downloadS3QuarantineBucketFiles(s3ObjectKey):
@@ -18,37 +33,48 @@ def downloadS3QuarantineBucketFiles(s3ObjectKey):
         Filename= ddanInDirFolder + s3ObjectKey
     )
 
-def listAllS3Objects():
+def listAllS3Objects(logger):
     s3Client = boto3.client('s3')
     s3ListObjectsv2Response = s3Client.list_objects_v2(
         Bucket=str(os.environ.get('S3_QUARANTINE_BUCKET_NAME'))
     )
+    logger.info("Downloading files from the designation S3 Quarantine Bucket...")
     print(str(s3ListObjectsv2Response))
-
     return s3ListObjectsv2Response["Contents"]
 
-def submitFilesToDDAN():
+def submitFilesToDDAN(logger):
     process = subprocess.Popen(
         ['~/ddan/dtascli', '-u'],
         stdout=subprocess.PIPE,
         universal_newlines=True
     )
     output = process.stdout.readline()
-    print(output.strip())
+    logger.info(output.strip())
 
 def main():
-    print("\n\nFile Storage Security - Push quarantine files to DDAN\n------------------------------------------------------------")
-    print(str(os.environ.get('S3_QUARANTINE_BUCKET_NAME')))
-    print(str(os.environ.get('DDAN_HOST')))
-    print(str(os.environ.get('DDAN_API_KEY')))
+    logger = create_timed_rotating_log("ddan-push.log")
+    logger.info("\n\nFile Storage Security - Push quarantine files to DDAN\n------------------------------------------------------------")
+    logger.info("Using OS Environment variables..")
+    logger.info("S3_QUARANTINE_BUCKET_NAME=" + str(os.environ.get('S3_QUARANTINE_BUCKET_NAME')))
+    logger.info("DDAN_HOST=" + str(os.environ.get('DDAN_HOST')))
+    logger.info("DDAN_API_KEY=" + str(os.environ.get('DDAN_API_KEY')))
 
-    # Clean up files in Deep Discovery Analyzer InDir directory
-    clearDdanInDir()
+    S3_QUARANTINE_BUCKET_NAME = str(os.environ.get('S3_QUARANTINE_BUCKET_NAME')) if 'S3_QUARANTINE_BUCKET_NAME' in os.environ else None
+    DDAN_HOST = str(os.environ.get('DDAN_HOST')) if 'DDAN_HOST' in os.environ else None
+    DDAN_API_KEY = str(os.environ.get('DDAN_API_KEY')) if 'DDAN_API_KEY' in os.environ else None
 
-    s3ObjList = listAllS3Objects()
+    if S3_QUARANTINE_BUCKET_NAME != None and DDAN_HOST != None and DDAN_API_KEY != None:
 
-    for s3Obj in s3ObjList:
-        print(str(s3Obj["Key"]))
-        downloadS3QuarantineBucketFiles(s3Obj["Key"])
+        # Clean up files in Deep Discovery Analyzer InDir directory
+        clearDdanInDir(logger)
 
-    submitFilesToDDAN()
+        s3ObjList = listAllS3Objects(logger)
+
+        for s3Obj in s3ObjList:
+            print(str(s3Obj["Key"]))
+            downloadS3QuarantineBucketFiles(s3Obj["Key"])
+
+        submitFilesToDDAN(logger)
+
+if __name__ == "__main__":
+    main()
